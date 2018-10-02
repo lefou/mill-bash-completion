f985ba2828fcbb690a52abcea7ac826c17f0fcd7
Tobias Roeser <le.petit.fou@web.de>
2018-10-02 15:44:48 +0200

    Create mill.complete.sh

---
+++ mill.complete.sh
@@ -0,0 +1,43 @@
+# bash completion for mill
+
+_mill()
+{
+    # variables
+    local cur prev split=false
+    local mill_out dot_count mill_resolve
+
+    COMPREPLY=()
+    _get_comp_words_by_ref -n : cur prev
+
+    _split_longopt && split=true
+
+    case $prev in
+        -h|--home|-p|--predef)
+            _filedir
+            return 0
+            ;;
+    esac
+
+    $split && return 0
+
+    if [[ "$cur" == -* ]]; then
+        COMPREPLY=( $( compgen -W '-c -h -p -s -w -i\
+          --code --color\
+          --disable-ticker\
+          --help --home\
+          --interactive\
+          --no-default-predef\
+          --predef\
+          --silent
+          --watch' -- "$cur" ) )
+    else
+
+        mill_out="$( mill --disable-ticker resolve __._ 2> /dev/null )"
+        COMPREPLY=( $( compgen -W "${mill_out}" -- "$cur" ) )
+
+    fi
+
+    __ltrim_colon_completions "${cur}"
+
+} &&
+complete -F _mill mill
